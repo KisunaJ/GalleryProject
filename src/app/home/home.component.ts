eea251ea75e01f1c63ad851628e9bed5036eb53e
KisunaJ <Jenny.Nieto@hotmail.es>
2020-06-29 17:44:56 -0300

    Se agrega Galeria de imagenes
    Se agrega galeria de imagenes en el componente home, se agregan arrays para la funcionalidad de la galeria y se establecen parametros de configuracion y funcionamiento de la galeria.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Imagen} from '../models/Images';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  imagenes: Imagen[] = [
+   
+    {
+      id: 1,
+      nombre: "Funeral peruano",
+      descripcion:"En este espacio estara la descripcion de la imagen que usamos arriba",
+      ruta: "assets/Images/funeral peruano.jpg",
+    },
+    {
+      id: 2,
+      nombre: "Fosas comunes",
+      descripcion:"En este espacio estara la descripcion de la imagen que usamos arriba",
+      ruta: "assets/Images/Fosas comunes.jpg",
+    },
+    {
+      id: 3,
+      nombre: "Hojas de coca",
+      descripcion:"En este espacio estara la descripcion de la imagen que usamos arriba",
+      ruta: "assets/Images/hojas de coca.jpg",
+    },
+    {
+      id: 4,
+      nombre: "Montañas de peru",
+      descripcion:"En este espacio estara la descripcion de la imagen que usamos arriba",
+      ruta: "assets/Images/montañas de peru.jpg",
+    },
+    {
+      id: 5,
+      nombre: "Ritual de despedida",
+      descripcion:"En este espacio estara la descripcion de la imagen que usamos arriba",
+      ruta: "assets/Images/ritual de despedida.jpg",
+    },
+    
+  ]
+
   
   constructor() { }
 
   ngOnInit(): void {
+
+
+
   }
 
 }
+
